560c08f8d1141b67fb39f5a2de3f0f917ffc0474
Tom Dickman <tomdickman@catalyst-au.net>
2021-02-10 11:03:06 +1100

    settings: add plugin settings and language strings

---
+++ lang/en/tool_pdfpages.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_pdfpages', language 'en'.
+ *
+ * @package    tool_pdfpages
+ * @author     Tom Dickman <tomdickman@catalyst-au.net>
+ * @copyright  2021 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'PDF Pages';
+$string['error:wkhtmltopdfpath'] = 'Cannot establish path to wkhtmltopdf, please check setting tool_pdfpages|wkhtmltopdfpath.';
+$string['error:urltopdf'] = 'URL to PDF conversion could not be completed.';
+$string['settings:wkhtmltopdfpath'] = 'wkhtmltopdf binary';
+$string['settings:wkhtmltopdfpath_desc'] = 'The path to wkhtmltopdf binary';
